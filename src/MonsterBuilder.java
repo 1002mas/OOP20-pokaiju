ab1d315e9caf90db9ab2badde7760f24bc9af1a2
Luca Barattini <luca.barattini4@studio.unibo.it>
2022-02-24 17:17:49 +0100

    created monsterBuilder

---
+++ src/MonsterBuilder.java
@@ -0,0 +1,30 @@
+
+public interface MonsterBuilder {
+
+	public MonsterBuilder name(String name); 
+	
+	public MonsterBuilder secondName(String name);
+	
+	public MonsterBuilder thirdName(String name);
+	
+	public MonsterBuilder fireType();
+	
+	public MonsterBuilder waterType();
+	
+	public MonsterBuilder grassType();
+
+	public MonsterBuilder level(int lvl);
+	
+	public MonsterBuilder health(int hp);
+	
+	public MonsterBuilder exp(int exp);
+	
+	public MonsterBuilder info(String info);
+	
+	public MonsterBuilder secondInfo(String info);
+	
+	public MonsterBuilder thirdInfo(String info);
+	
+	public Monster build();
+	
+}
