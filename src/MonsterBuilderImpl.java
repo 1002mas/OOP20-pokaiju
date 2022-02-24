7bc5a299f85f3face3f48f6ae5c8014696a300c3
Luca Barattini <luca.barattini4@studio.unibo.it>
2022-02-24 18:16:53 +0100

    monster builder implementation

--- src/MonsterBuilderImpl.java
+++ src/MonsterBuilderImpl.java
@@ -1,7 +1,6 @@
 
-public class MonsterBuilderImpl implements MonsterBuilder{
+public class MonsterBuilderImpl implements MonsterBuilder {
 
-	
 	private static final int EXP_CAP = 1000;
 	private String name;
 	private MonsterType type;
@@ -18,70 +17,71 @@ public class MonsterBuilderImpl implements MonsterBuilder{
 		this.name = name;
 		return this;
 	}
-	
+
 	public MonsterBuilder secondName(String name) {
 		this.firstEvolution = name;
 		return this;
 	}
-	
+
 	public MonsterBuilder thirdName(String name) {
 		this.secondEvolution = name;
 		return this;
 	}
-	
+
 	public MonsterBuilder monsterType(MonsterType type) {
 		this.type = type;
 		return this;
 	}
-	
 
 	public MonsterBuilder level(int lvl) {
-		if(lvl >= 100 || lvl < 0) {
+		if (lvl <= 0 || lvl > 100 ) {
 			this.level = 1;
 			return this;
 		}
 		this.level = lvl;
 		return this;
 	}
-	
+
 	public MonsterBuilder health(int hp) {
-		if(hp >= 100 || hp < 0) {
-			this.level = 1;
+		if ( hp < 0) {
+			this.health = 1;
 			return this;
 		}
-		this.level = hp;
+		this.health = hp;
 		return this;
 	}
-	
+
 	public MonsterBuilder exp(int exp) {
-		if(exp >= EXP_CAP || exp < 0) {
+		if (exp >= EXP_CAP || exp < 0) {
 			this.exp = EXP_CAP;
 			return this;
 		}
-		this.level = exp;
+		this.exp = exp;
 		return this;
 	}
-	
+
 	public MonsterBuilder info(String info) {
 		this.info = info;
 		return this;
 	}
-	
+
 	public MonsterBuilder secondInfo(String info) {
 		this.secondInfo = info;
 		return this;
 	}
-	
+
 	public MonsterBuilder thirdInfo(String info) {
 		this.thirdInfo = info;
 		return this;
 	}
-	
-	public Monster build() throws IllegalStateException {
-		if (this.name == null || this.type == null || this.info == null || this.secondInfo == null || this.thirdInfo == null || this.firstEvolution == null || this.secondEvolution == null) {
+
+	public Monster build(){
+		if (this.name == null || this.type == null || this.info == null || this.secondInfo == null
+				|| this.thirdInfo == null || this.firstEvolution == null || this.secondEvolution == null) {
 			throw new IllegalStateException("");
 		}
-		return new MonsterImpl(name, type, level, health, exp, info, firstEvolution, secondInfo, secondEvolution, thirdInfo);
+		return new MonsterImpl(this.name, this.type, this.level, this.health, this.exp, this.info, this.firstEvolution, this.secondInfo, this.secondEvolution,
+				this.thirdInfo);
 	}
 
 }
