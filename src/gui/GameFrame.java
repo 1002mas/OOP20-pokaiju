9f3cbe528194318164d0decbebf1be8849971cce
Carafassi Samuele <samuele.carafassi@studio.unibo.it>
2022-03-23 14:44:08 +0100

    added panel creation functions to gui

--- src/gui/GameFrame.java
+++ src/gui/GameFrame.java
@@ -1,9 +1,36 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import controller.PlayerController;
 
 public class GameFrame extends JFrame {
 
     private static final long serialVersionUID = -7927156597267134363L;
+    private /* final */ PlayerController playerController;
 
+    public GameFrame(PlayerController playerController) {
+	this.playerController = playerController;
+	/*
+	 * TODO set size, cardLayout, first menu (panel 1) 
+	 * */
+	
+    }
+
+    //TODO create player + map (panel 2)
+    private JPanel buildMapPanel() {
+	return new JPanel();
+    }
+
+    //TODO create battle scene (panel 3)
+    private JPanel buildBattlePanel() {
+	return new JPanel();
+    }
+
+    //TODO create menu (panel 4)
+    private JPanel buildMenuPanel() {
+	return new JPanel();
+    }
+    private void changePanel(String name) {}
 }
