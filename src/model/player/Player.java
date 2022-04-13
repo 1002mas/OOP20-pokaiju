c0ad3713a19ec779f489b1c08c0c7417efd0adff
Guo jiahao <jiahao.guo@studio.unibo.it>
2022-04-14 00:24:03 +0200

    added player movement in Player model and added & updated javadoc

--- src/model/player/Player.java
+++ src/model/player/Player.java
@@ -5,7 +5,9 @@ import java.util.Map;
 
 import model.Pair;
 import model.gameitem.*;
+import model.map.GameMap;
 import model.monster.Monster;
+import model.monster.MonsterSpecies;
 
 public interface Player {
     /**
@@ -159,5 +161,43 @@ public interface Player {
      * @param Monster, GameItem to be used
      */
     void evolveMonster(Monster monster, GameItem i);
+
+    /**
+     * This function gets the EvolutionList
+     * 
+     * @return List of evolvedMonsters and base
+     */
+    List<Pair<MonsterSpecies, MonsterSpecies>> getEvolutionList();
+
+    /**
+     * Moves Player up
+     * 
+     */
+    boolean moveUp();
+
+    /**
+     * Moves Player down
+     * 
+     */
+    boolean moveDown();
+
+    /**
+     * Moves Player left
+     * 
+     */
+    boolean moveLeft();
+
+    /**
+     * Moves Player right
+     * 
+     */
+    boolean moveRight();
+
+    /**
+     * This function gets the GameMap
+     * 
+     * @return map
+     */
+    GameMap getMap();
 
 }
