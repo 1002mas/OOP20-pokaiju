9b10850de93940753583b12881abe80b34125491
Andrea <andrea.castorina@studio.unibo.it>
2022-04-15 16:35:41 +0200

    added function in monsterStorageImpl

--- src/model/player/MonsterStorageImpl.java
+++ src/model/player/MonsterStorageImpl.java
@@ -8,13 +8,19 @@ import model.monster.Monster;
 public class MonsterStorageImpl implements MonsterStorage {
 
 	private static final int MAX_NUMBER_OF_BOX = 10;
+	private static final int MAX_SIZE_OF_BOX = 10;
 	private static final String INITIAL_BOX_NAME = "BOX";
 
 	private List<MonsterBox> monsterBoxes;
 	private int currentMonsterBoxIndex;
 	private Player player;
 
-	public MonsterStorageImpl(Player player, List<MonsterBox> boxes) {
+	//TODO max monster fuori
+	//TODO add monster
+	//TODO Costruttore
+	//TODO liste circolari
+	
+	public  MonsterStorageImpl(Player player, List<MonsterBox> boxes) {
 		this.player = player;
 		this.monsterBoxes = new ArrayList<>(boxes);
 		this.currentMonsterBoxIndex = 1;
@@ -24,20 +30,46 @@ public class MonsterStorageImpl implements MonsterStorage {
 			generateBoxs(MAX_NUMBER_OF_BOX - monsterBoxes.size());
 		}
 	}
+	
+	public MonsterStorageImpl(Player player) {
+		this.player = player;
+		this.currentMonsterBoxIndex = 1;
+		this.monsterBoxes = new ArrayList<>();
+		generateBoxs(MAX_NUMBER_OF_BOX);
+		
+	}
+	
+	
 
 	private void generateBoxs(int n) {
 		MonsterBox box;
+		
 		for (int i = n; i < MAX_NUMBER_OF_BOX; i++) {
-			box = new MonsterBoxImpl(INITIAL_BOX_NAME + i);
+			box = new MonsterBoxImpl(INITIAL_BOX_NAME + i, MAX_SIZE_OF_BOX);
 			this.monsterBoxes.add(box);
 		}
 	}
-
+	private MonsterBox getFirstBoxFree(){
+		for(int i = 1; i< MAX_NUMBER_OF_BOX; i++) {
+			if(!this.monsterBoxes.get(i).isFull()) {
+				return this.monsterBoxes.get(i);
+			}
+			
+		}
+		return null;
+	}
 	@Override
 	public void addMonster(Monster monster) {
-		if (!getCurrentBox().isFull()) {
-			getCurrentBox().addMonster(monster);
+		if(!this.getCurrentBox().isFull()) {
+			this.getCurrentBox().addMonster(monster);
 		}
+		else {	
+			MonsterBox monsterBox = getFirstBoxFree();			
+			if(monsterBox!=null) {
+				monsterBox.addMonster(monster);
+			}
+		}
+		
 	}
 
 	@Override
@@ -86,9 +118,7 @@ public class MonsterStorageImpl implements MonsterStorage {
 
 	@Override
 	public void nextBox() {
-		if (this.currentMonsterBoxIndex < MAX_NUMBER_OF_BOX) {
-			this.currentMonsterBoxIndex++;
-		}
+		this.currentMonsterBoxIndex = (this.currentMonsterBoxIndex + 1)%MAX_SIZE_OF_BOX;
 
 	}
 
@@ -107,5 +137,13 @@ public class MonsterStorageImpl implements MonsterStorage {
 	@Override
 	public List<Monster> getCurrentBoxMonsters() {
 		return getCurrentBox().getAllMonsters();
+	}
+	
+	public int getMaxSizeOfBox() {
+		return MAX_SIZE_OF_BOX;
+	}
+	
+	public int currentBoxSize() {
+		return this.getCurrentBoxMonsters().size();
 	}
 }
