bf9275ba987b39e192b05ccd3716df7cf3a5fd3b
Guo jiahao <jiahao.guo@studio.unibo.it>
2022-04-19 23:17:07 +0200

    revolved some checkstyle

--- src/gui/GameFrameImpl.java
+++ src/gui/GameFrameImpl.java
@@ -56,39 +56,39 @@ public class GameFrameImpl extends JFrame implements GameFrame {
     private final PlayerController playerController;
 
     public GameFrameImpl(final PlayerController playerController) {
-	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	this.setTitle("POKAIJU");
-	final Font myFont = new Font("Bold", 0, 16);
-	final UIDefaults defaultUI = UIManager.getDefaults();
-	defaultUI.put("Button.font", myFont);
-	defaultUI.put("Label.font", myFont);
-	defaultUI.put("ComboBox.font", myFont);
-	defaultUI.put("TextArea.font", myFont);
-	defaultUI.put("TextField.font", myFont);
-	this.playerController = playerController;
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("POKAIJU");
+        final Font myFont = new Font("Bold", 0, 16);
+        final UIDefaults defaultUI = UIManager.getDefaults();
+        defaultUI.put("Button.font", myFont);
+        defaultUI.put("Label.font", myFont);
+        defaultUI.put("ComboBox.font", myFont);
+        defaultUI.put("TextArea.font", myFont);
+        defaultUI.put("TextField.font", myFont);
+        this.playerController = playerController;
 
-	size = getMainPanelSize();
-	mainPanel.setPreferredSize(new Dimension(size, size));
-	mainPanel.setLayout(cLayout);
-	imgLoad = new ImagesLoaderImpl(size, size, playerController.getMaximumBlocksInRow(),
-		playerController.getMaximumBlocksInColumn());
+        size = getMainPanelSize();
+        mainPanel.setPreferredSize(new Dimension(size, size));
+        mainPanel.setLayout(cLayout);
+        imgLoad = new ImagesLoaderImpl(size, size, playerController.getMaximumBlocksInRow(),
+                playerController.getMaximumBlocksInColumn());
 
-	final LoginPanel loginPanel = new LoginPanel();
+        final LoginPanel loginPanel = new LoginPanel();
 
-	loginPanel.getnewGame().addActionListener(e -> updateView(NEW_GAME_VIEW));
-	loginPanel.getquitGame().addActionListener(e -> System.exit(0));
+        loginPanel.getnewGame().addActionListener(e -> updateView(NEW_GAME_VIEW));
+        loginPanel.getquitGame().addActionListener(e -> System.exit(0));
 
-	final JPanel newGamePanel = newGamePanel();
+        final JPanel newGamePanel = newGamePanel();
 
-	mainPanel.add(loginPanel, LOGIN_VIEW);
-	mainPanel.add(newGamePanel, NEW_GAME_VIEW);
+        mainPanel.add(loginPanel, LOGIN_VIEW);
+        mainPanel.add(newGamePanel, NEW_GAME_VIEW);
 
-	subPanels.put(LOGIN_VIEW, loginPanel);
-	subPanels.put(NEW_GAME_VIEW, newGamePanel);
-	this.setResizable(false);
-	this.setContentPane(mainPanel);
-	this.pack();
-	this.setVisible(true);
+        subPanels.put(LOGIN_VIEW, loginPanel);
+        subPanels.put(NEW_GAME_VIEW, newGamePanel);
+        this.setResizable(false);
+        this.setContentPane(mainPanel);
+        this.pack();
+        this.setVisible(true);
 
     }
 
@@ -98,11 +98,11 @@ public class GameFrameImpl extends JFrame implements GameFrame {
      * @return the main panel size
      */
     private int getMainPanelSize() {
-	final double percScreen = 5.0 / 6.0;
-	final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	int s = (int) (screenSize.getHeight() > screenSize.getWidth() ? screenSize.getWidth() : screenSize.getHeight());
-	s = (int) (s * percScreen);
-	return s;
+        final double percScreen = 5.0 / 6.0;
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int s = (int) (screenSize.getHeight() > screenSize.getWidth() ? screenSize.getWidth() : screenSize.getHeight());
+        s = (int) (s * percScreen);
+        return s;
     }
 
     /**
@@ -110,19 +110,19 @@ public class GameFrameImpl extends JFrame implements GameFrame {
      * @return the panel with map, player and npc
      */
     private JPanel buildMapPanel() {
-	final TwoLayersPanel mapPanel = new TwoLayersPanel(playerController, imgLoad, size, size);
-	mapPanel.addKeyListener(new PlayerCommands(this));
-	mapPanel.addFocusListener(new FocusListener() {
-	    @Override
-	    public void focusLost(final FocusEvent e) {
-		mapPanel.requestFocusInWindow();
-	    }
+        final TwoLayersPanel mapPanel = new TwoLayersPanel(playerController, imgLoad, size, size);
+        mapPanel.addKeyListener(new PlayerCommands(this));
+        mapPanel.addFocusListener(new FocusListener() {
+            @Override
+            public void focusLost(final FocusEvent e) {
+                mapPanel.requestFocusInWindow();
+            }
 
-	    @Override
-	    public void focusGained(final FocusEvent e) {
-	    }
-	});
-	return mapPanel;
+            @Override
+            public void focusGained(final FocusEvent e) {
+            }
+        });
+        return mapPanel;
     }
 
     /**
@@ -131,30 +131,30 @@ public class GameFrameImpl extends JFrame implements GameFrame {
      */
     @Override
     public void movePlayer(final Direction dir) {
-	final TwoLayersPanel p = (TwoLayersPanel) subPanels.get(MAP_VIEW);
-	final PlayerPanel topPanel = p.getTopPanel();
-	boolean animationOn = true;
-	final boolean canPlayerMove = this.playerController.movePlayer(dir);
-	if (canPlayerMove) {
-	    topPanel.setNextPosition(this.playerController.getPlayerPosition());
-	    if (playerController.hasPlayerChangedMap()) {
-		final List<BufferedImage> mapImageSequence = imgLoad
-			.getMapByID(this.playerController.getCurrentMapID());
-		p.setMapImage(mapImageSequence);
-		topPanel.setNpcs(this.playerController.getAllNpcs());
-		animationOn = false;
-	    }
-	}
+        final TwoLayersPanel p = (TwoLayersPanel) subPanels.get(MAP_VIEW);
+        final PlayerPanel topPanel = p.getTopPanel();
+        boolean animationOn = true;
+        final boolean canPlayerMove = this.playerController.movePlayer(dir);
+        if (canPlayerMove) {
+            topPanel.setNextPosition(this.playerController.getPlayerPosition());
+            if (playerController.hasPlayerChangedMap()) {
+                final List<BufferedImage> mapImageSequence = imgLoad
+                        .getMapByID(this.playerController.getCurrentMapID());
+                p.setMapImage(mapImageSequence);
+                topPanel.setNpcs(this.playerController.getAllNpcs());
+                animationOn = false;
+            }
+        }
 
-	if (animationOn) {
-	    topPanel.animatedMove(dir, canPlayerMove);
-	} else {
-	    topPanel.staticMove();
-	}
-	changeToBattle();
-	if (playerController.hasPlayerTriggeredEvent()) {
-	    topPanel.setNpcs(this.playerController.getAllNpcs());
-	}
+        if (animationOn) {
+            topPanel.animatedMove(dir, canPlayerMove);
+        } else {
+            topPanel.staticMove();
+        }
+        changeToBattle();
+        if (playerController.hasPlayerTriggeredEvent()) {
+            topPanel.setNpcs(this.playerController.getAllNpcs());
+        }
     }
 
     /**
@@ -163,13 +163,13 @@ public class GameFrameImpl extends JFrame implements GameFrame {
      * @return true if it changed to battle view
      */
     private boolean changeToBattle() {
-	if (playerController.hasBattleStarted()) {
-	    final BattlePanel b = (BattlePanel) (this.subPanels.get(BATTLE_VIEW));
-	    b.setBattleController(this.playerController.getBattleController().get(), this.playerController);
-	    updateView(BATTLE_VIEW);
-	    return true;
-	}
-	return false;
+        if (playerController.hasBattleStarted()) {
+            final BattlePanel b = (BattlePanel) (this.subPanels.get(BATTLE_VIEW));
+            b.setBattleController(this.playerController.getBattleController().get(), this.playerController);
+            updateView(BATTLE_VIEW);
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -178,17 +178,17 @@ public class GameFrameImpl extends JFrame implements GameFrame {
      */
     @Override
     public boolean playerInteraction() {
-	final TwoLayersPanel p = (TwoLayersPanel) subPanels.get(MAP_VIEW);
-	final PlayerPanel topPanel = p.getTopPanel();
-	final Optional<String> text = playerController.interact();
-	if (text.isPresent()) {
-	    topPanel.showText(playerController.getNpcName().get().toUpperCase() + ": " + text.get());
+        final TwoLayersPanel p = (TwoLayersPanel) subPanels.get(MAP_VIEW);
+        final PlayerPanel topPanel = p.getTopPanel();
+        final Optional<String> text = playerController.interact();
+        if (text.isPresent()) {
+            topPanel.showText(playerController.getNpcName().get().toUpperCase() + ": " + text.get());
 
-	}
-	if (playerController.hasPlayerTriggeredEvent()) {
-	    topPanel.setNpcs(this.playerController.getAllNpcs());
-	}
-	return text.isEmpty();
+        }
+        if (playerController.hasPlayerTriggeredEvent()) {
+            topPanel.setNpcs(this.playerController.getAllNpcs());
+        }
+        return text.isEmpty();
     }
 
     /**
@@ -197,113 +197,113 @@ public class GameFrameImpl extends JFrame implements GameFrame {
      */
     @Override
     public void endPlayerInteraction() {
-	final TwoLayersPanel p = (TwoLayersPanel) subPanels.get(MAP_VIEW);
-	final PlayerPanel topPanel = p.getTopPanel();
-	topPanel.hideText();
-	changeToBattle();
-	if (this.playerController.hasMerchantInteractionOccurred()) {
-	    updateView(MERCHANT_VIEW);
-	}
+        final TwoLayersPanel p = (TwoLayersPanel) subPanels.get(MAP_VIEW);
+        final PlayerPanel topPanel = p.getTopPanel();
+        topPanel.hideText();
+        changeToBattle();
+        if (this.playerController.hasMerchantInteractionOccurred()) {
+            updateView(MERCHANT_VIEW);
+        }
     }
 
     private JPanel buildMenuPanel() {
-	return new MenuPanel(playerController, imgLoad, size, this);
+        return new MenuPanel(playerController, imgLoad, size, this);
     }
 
     private JPanel newGamePanel() {
-	final JPanel panel = new JPanel(new BorderLayout());
-	panel.setBorder(BorderFactory.createLineBorder(Color.green));
+        final JPanel panel = new JPanel(new BorderLayout());
+        panel.setBorder(BorderFactory.createLineBorder(Color.green));
 
-	final JLabel nameLabel = new JLabel();
-	final JTextField nameField = new JTextField(10);
-	nameLabel.setText("Insert name :");
+        final JLabel nameLabel = new JLabel();
+        final JTextField nameField = new JTextField(10);
+        nameLabel.setText("Insert name :");
 
-	final JLabel genderLabel = new JLabel();
-	String[] genderText = new String[this.playerController.getGender().size()];
-	for (int i = 0; i < this.playerController.getGender().size(); i++) {
-	    genderText[i] = this.playerController.getGender().get(i);
-	}
-	final JComboBox<String> gender = new JComboBox<>(genderText);
-	genderLabel.setText("Select your gender :");
+        final JLabel genderLabel = new JLabel();
+        String[] genderText = new String[this.playerController.getGender().size()];
+        for (int i = 0; i < this.playerController.getGender().size(); i++) {
+            genderText[i] = this.playerController.getGender().get(i);
+        }
+        final JComboBox<String> gender = new JComboBox<>(genderText);
+        genderLabel.setText("Select your gender :");
 
-	final JLabel trainerNumberLabel = new JLabel();
-	trainerNumberLabel.setText("Trainer number is generated randomly : ");
-	final JTextField trainerNumberField = new JTextField();
-	trainerNumberField.setEditable(false);
-	trainerNumberLabel.setEnabled(false);
+        final JLabel trainerNumberLabel = new JLabel();
+        trainerNumberLabel.setText("Trainer number is generated randomly : ");
+        final JTextField trainerNumberField = new JTextField();
+        trainerNumberField.setEditable(false);
+        trainerNumberLabel.setEnabled(false);
 
-	final JButton postData = new JButton("CREATE");
-	postData.addActionListener(e -> {
-	    if (nameField.getText().equals("")) {
-		JOptionPane.showMessageDialog(null, "Name can't be null", "alert", JOptionPane.WARNING_MESSAGE);
-	    } else {
-		playerController.createNewPlayer(nameField.getText(), gender.getSelectedItem().toString(),
-			Integer.parseInt(trainerNumberField.getText()));
+        final JButton postData = new JButton("CREATE");
+        postData.addActionListener(e -> {
+            if ("".equals(nameField.getText())) {
+                JOptionPane.showMessageDialog(null, "Name can't be null", "alert", JOptionPane.WARNING_MESSAGE);
+            } else {
+                playerController.createNewPlayer(nameField.getText(), gender.getSelectedItem().toString(),
+                        Integer.parseInt(trainerNumberField.getText()));
 
-		final JPanel gamePanel = buildMapPanel();
-		final JPanel menuPanel = buildMenuPanel();
-		final JPanel battlePanel = new BattlePanel(imgLoad, this);
-		final JPanel merchantPanel = new MerchantPanel(this, playerController);
-		mainPanel.add(gamePanel, MAP_VIEW);
-		mainPanel.add(battlePanel, BATTLE_VIEW);
-		mainPanel.add(menuPanel, MENU_VIEW);
-		mainPanel.add(merchantPanel, MERCHANT_VIEW);
-		subPanels.put(MERCHANT_VIEW, merchantPanel);
-		subPanels.put(MENU_VIEW, menuPanel);
-		subPanels.put(BATTLE_VIEW, battlePanel);
-		subPanels.put(MAP_VIEW, gamePanel);
+                final JPanel gamePanel = buildMapPanel();
+                final JPanel menuPanel = buildMenuPanel();
+                final JPanel battlePanel = new BattlePanel(imgLoad, this);
+                final JPanel merchantPanel = new MerchantPanel(this, playerController);
+                mainPanel.add(gamePanel, MAP_VIEW);
+                mainPanel.add(battlePanel, BATTLE_VIEW);
+                mainPanel.add(menuPanel, MENU_VIEW);
+                mainPanel.add(merchantPanel, MERCHANT_VIEW);
+                subPanels.put(MERCHANT_VIEW, merchantPanel);
+                subPanels.put(MENU_VIEW, menuPanel);
+                subPanels.put(BATTLE_VIEW, battlePanel);
+                subPanels.put(MAP_VIEW, gamePanel);
 
-		updateView(GameFrameImpl.MAP_VIEW);
-	    }
-	});
+                updateView(GameFrameImpl.MAP_VIEW);
+            }
+        });
 
-	final JButton quitButton = new JButton("BACK TO MENU");
-	quitButton.addActionListener(e -> updateView(GameFrameImpl.LOGIN_VIEW));
+        final JButton quitButton = new JButton("BACK TO MENU");
+        quitButton.addActionListener(e -> updateView(GameFrameImpl.LOGIN_VIEW));
 
-	final JPanel topPanel = new JPanel(new FlowLayout());
-	topPanel.add(quitButton, FlowLayout.LEFT);
+        final JPanel topPanel = new JPanel(new FlowLayout());
+        topPanel.add(quitButton, FlowLayout.LEFT);
 
-	final JPanel underPanel = new JPanel(new GridBagLayout());
+        final JPanel underPanel = new JPanel(new GridBagLayout());
 
-	GridBagConstraints rows = new GridBagConstraints();
-	rows.gridy = 1;
-	rows.insets = new Insets(0, 0, SPACE, 0);
-	rows.fill = GridBagConstraints.BOTH;
-	underPanel.add(nameLabel, rows);
-	underPanel.add(nameField, rows);
-	rows.gridy++;
-	underPanel.add(genderLabel, rows);
-	underPanel.add(gender, rows);
-	rows.gridy++;
-	underPanel.add(trainerNumberLabel, rows);
-	underPanel.add(trainerNumberField, rows);
-	rows.gridy++;
-	underPanel.add(postData, rows);
+        GridBagConstraints rows = new GridBagConstraints();
+        rows.gridy = 1;
+        rows.insets = new Insets(0, 0, SPACE, 0);
+        rows.fill = GridBagConstraints.BOTH;
+        underPanel.add(nameLabel, rows);
+        underPanel.add(nameField, rows);
+        rows.gridy++;
+        underPanel.add(genderLabel, rows);
+        underPanel.add(gender, rows);
+        rows.gridy++;
+        underPanel.add(trainerNumberLabel, rows);
+        underPanel.add(trainerNumberField, rows);
+        rows.gridy++;
+        underPanel.add(postData, rows);
 
-	panel.addComponentListener(new ComponentListener() {
-	    @Override
-	    public void componentShown(ComponentEvent e) {
-		final Random rand = new Random();
-		final int a = rand.nextInt(999_999) + 100_000;
-		trainerNumberField.setText(Integer.toString(a));
-		nameField.setText("");
-	    }
+        panel.addComponentListener(new ComponentListener() {
+            @Override
+            public void componentShown(final ComponentEvent e) {
+                final Random rand = new Random();
+                final int a = rand.nextInt(999_999) + 100_000;
+                trainerNumberField.setText(Integer.toString(a));
+                nameField.setText("");
+            }
 
-	    @Override
-	    public void componentResized(ComponentEvent e) {
-	    }
+            @Override
+            public void componentResized(final ComponentEvent e) {
+            }
 
-	    @Override
-	    public void componentMoved(ComponentEvent e) {
-	    }
+            @Override
+            public void componentMoved(final ComponentEvent e) {
+            }
 
-	    @Override
-	    public void componentHidden(ComponentEvent e) {
-	    }
-	});
-	panel.add(topPanel, BorderLayout.NORTH);
-	panel.add(underPanel, BorderLayout.CENTER);
-	return panel;
+            @Override
+            public void componentHidden(final ComponentEvent e) {
+            }
+        });
+        panel.add(topPanel, BorderLayout.NORTH);
+        panel.add(underPanel, BorderLayout.CENTER);
+        return panel;
     }
 
     /**
@@ -311,8 +311,8 @@ public class GameFrameImpl extends JFrame implements GameFrame {
      */
     @Override
     public void updateView(final String name) {
-	cLayout.show(mainPanel, name);
-	subPanels.get(name).requestFocusInWindow();
+        cLayout.show(mainPanel, name);
+        subPanels.get(name).requestFocusInWindow();
     }
 
 }
