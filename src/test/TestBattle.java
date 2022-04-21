046ca741c8817689c14c3c001bc56142b2a7a684
Michael Pierantoni <michael.pierantoni2@studio.unibo.it>
2022-04-21 17:26:26 +0200

    fixed JUnit TestBattle

--- src/test/TestBattle.java
+++ src/test/TestBattle.java
@@ -27,23 +27,24 @@ public class TestBattle {
     private Monster wildMonster;
     private NpcTrainer enemyTrainer;
     private Player player;
-
+    private static final int ATTACK = 50;
+    private static final int HEALTH = 40;
+    private static final int SPEED = 30;
+    private static final int DEFENSE = 20;
+    private static final int FIRST_LEVEL = 1;
+    private static final int EXP_BASE = 0;
+    private static final int PP = 10;
     @org.junit.Before
     public void init() {
-        final int fifty = 50;
-        final int twenty = 20;
-        final int ten = 10;
-        final int zero = 0;
-        final int one = 1;
         List<Pair<Moves, Integer>> firstListOfMoves;
         List<Pair<Moves, Integer>> secondListOfMoves;
         List<Moves> firstMonsterSpeciesMoves;
         List<Moves> secondMonsterSpeciesMoves;
 
-        final Moves m1 = new MovesImpl("Braciere", fifty, MonsterType.FIRE, ten);
-        final Moves m2 = new MovesImpl("Attacco", ten, MonsterType.FIRE, ten);
-        final Moves m3 = new MovesImpl("Volo", twenty, MonsterType.FIRE, ten);
-        final Moves m4 = new MovesImpl("Fossa", ten, MonsterType.FIRE, ten);
+        final Moves m1 = new MovesImpl("Braciere", ATTACK, MonsterType.FIRE, PP);
+        final Moves m2 = new MovesImpl("Attacco", ATTACK, MonsterType.FIRE, PP);
+        final Moves m3 = new MovesImpl("Volo", ATTACK, MonsterType.FIRE, PP);
+        final Moves m4 = new MovesImpl("Fossa", ATTACK, MonsterType.FIRE, PP);
 
         firstListOfMoves = List.of(new Pair<>(m1, 10), new Pair<>(m2, 10));
         secondListOfMoves = List.of(new Pair<>(m3, 10), new Pair<>(m4, 10));
@@ -51,29 +52,29 @@ public class TestBattle {
         secondMonsterSpeciesMoves = List.of(m3, m4);
 
         MonsterSpecies species = new MonsterSpeciesBuilderImpl().name("bibol").info("Info1")
-                .monsterType(MonsterType.FIRE).health(fifty).attack(ten).defense(ten).speed(ten)
+                .monsterType(MonsterType.FIRE).health(HEALTH).attack(PP).defense(DEFENSE).speed(PP)
                 .movesList(firstMonsterSpeciesMoves).build();
 
-        final Monster playerMonster1 = new MonsterBuilderImpl().health(fifty).attack(fifty).defense(fifty).speed(fifty)
-                .exp(zero).level(one).wild(false).species(species).movesList(firstListOfMoves).build();
+        final Monster playerMonster1 = new MonsterBuilderImpl().health(HEALTH).attack(ATTACK * 2).defense(DEFENSE).speed(SPEED)
+                .exp(EXP_BASE).level(FIRST_LEVEL).wild(false).species(species).movesList(firstListOfMoves).build();
 
         species = new MonsterSpeciesBuilderImpl().name("greyfish").info("Info").monsterType(MonsterType.WATER)
-                .health(fifty).attack(10).defense(10).speed(10).movesList(firstMonsterSpeciesMoves).build();
+                .health(ATTACK).attack(10).defense(10).speed(10).movesList(firstMonsterSpeciesMoves).build();
 
-        this.playerMonster2 = new MonsterBuilderImpl().health(fifty).attack(fifty).defense(fifty).speed(fifty).exp(zero)
-                .level(one).wild(false).species(species).movesList(firstListOfMoves).build();
+        this.playerMonster2 = new MonsterBuilderImpl().health(HEALTH).attack(ATTACK).defense(DEFENSE).speed(SPEED).exp(EXP_BASE)
+                .level(FIRST_LEVEL).wild(false).species(species).movesList(firstListOfMoves).build();
 
         species = new MonsterSpeciesBuilderImpl().name("kracez").info("Info").monsterType(MonsterType.FIRE)
-                .health(fifty).attack(10).defense(10).speed(10).movesList(secondMonsterSpeciesMoves).build();
+                .health(ATTACK).attack(10).defense(10).speed(10).movesList(secondMonsterSpeciesMoves).build();
 
-        this.wildMonster = new MonsterBuilderImpl().health(fifty).attack(fifty).defense(fifty).speed(fifty).exp(zero)
-                .level(one).wild(true).species(species).movesList(secondListOfMoves).build();
+        this.wildMonster = new MonsterBuilderImpl().health(10).attack(ATTACK).defense(0).speed(SPEED).exp(EXP_BASE)
+                .level(FIRST_LEVEL).wild(true).species(species).movesList(secondListOfMoves).build();
 
         species = new MonsterSpeciesBuilderImpl().name("pirin").info("Info").monsterType(MonsterType.GRASS)
-                .health(fifty).attack(10).defense(10).speed(10).movesList(secondMonsterSpeciesMoves).build();
+                .health(ATTACK).attack(10).defense(10).speed(10).movesList(secondMonsterSpeciesMoves).build();
 
-        final Monster enemyTrainerMonster = new MonsterBuilderImpl().health(fifty).attack(fifty).defense(fifty)
-                .speed(fifty).exp(zero).level(one).wild(true).species(species).movesList(secondListOfMoves).build();
+        final Monster enemyTrainerMonster = new MonsterBuilderImpl().health(HEALTH).attack(ATTACK).defense(DEFENSE)
+                .speed(SPEED).exp(EXP_BASE).level(FIRST_LEVEL).wild(true).species(species).movesList(secondListOfMoves).build();
 
         this.player = new PlayerImpl("Paolo", Gender.MAN, 0, new Pair<>(0, 0), null);
         this.player.addMonster(playerMonster1);
@@ -87,7 +88,7 @@ public class TestBattle {
 
         final MonsterBattleImpl battle = new MonsterBattleImpl(player, wildMonster);
         battle.movesSelection(0);
-        assertFalse(wildMonster.isAlive());
+        assertTrue(wildMonster.isAlive());
     }
 
     @org.junit.Test
@@ -121,7 +122,7 @@ public class TestBattle {
     public void battleWithEnemyTrainer() {
         final MonsterBattleImpl battle = new MonsterBattleImpl(player, enemyTrainer);
         battle.movesSelection(0);
-        assertTrue(enemyTrainer.isDefeated());
+        assertFalse(enemyTrainer.isDefeated());
     }
 
 }
